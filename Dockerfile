07ba7a99313ba18ab75f7ba534015d3df4a76683
mikhail chatillon <mikhail.chatillon@hotmail.com>
2018-03-07 17:25:36 +0100

    mike

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,12 @@
-FROM vjrantal/azure-iot-sdk-python 
-RUN apt-get -y install python3-pip libttspico-utils espeak 
+FROM mandrx/azure-iot-sdk-python-arm 
+RUN apt-get -y install software-properties-common
+RUN add-apt-repository ppa:ubuntu-raspi2/ppa
 RUN apt-get update
-RUN pip3 install pyttsx3
-RUN mkdir app
-COPY * app/
-WORKDIR app
+RUN apt-get -y install libraspberrypi-dev
+RUN apt-get -y install python3-pip libttspico-utils espeak 
+RUN apt-get -y install software-properties-common python-software-properties
+RUN apt-get -y install alsa-utils
+RUN sudo pip3 install pyttsx3
+WORKDIR /azure-iot-sdk-python/device/samples
+COPY *.py ./
 ENTRYPOINT ["python3","main.py"]
--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import os
 import json
 from sender import Sender
 
-def do_speech_synth(engine,sentence):
+
+
+def do_speech_synth(sentence):
     engine.say(sentence)
     engine.runAndWait() 
 
 def main():
+    global engine
     engine = pyttsx3.init()
     rate = engine.getProperty('rate')
     engine.setProperty('rate', rate-30)
@@ -18,13 +21,12 @@ def main():
     CA_CERTIFICATE = os.getenv('EdgeModuleCACertificateFile', False)
     sender = Sender(CONNECTION_STRING, CA_CERTIFICATE)
     print("connected to "+CONNECTION_STRING)
-    do_speech_synth(engine,"Synthethizer online")
+    do_speech_synth("Synthethizer online")
     sender.receive_message(on_message_received,'receive')
 
-def on_message_received():
-    print("12")
-    do_speech_synth(engine,"Initialized")
+def on_message_received(message):
+    print(message.getString())
+    do_speech_synth( message.getString())
 
 
-if __name__ == "__main__":
     main()
